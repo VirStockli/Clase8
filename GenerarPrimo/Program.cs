003e45f1dca84021da404af996a7ea99ae9e1b35
VirStockli <virstockli@gmail.com>
2024-05-16 17:23:52 -0300

    Actualizar GenerarPrimos

---
+++ GenerarPrimo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerarPrimo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solicitar desde y hasta");
+            int desde = Convert.ToInt32(Console.ReadLine());
+            int hasta = Convert.ToInt32(Console.ReadLine());
+
+            for (int n = desde; n <= hasta; n++) 
+            {
+                Console.WriteLine("ingrese el numero");
+
+                int numero = Convert.ToInt32(Console.ReadLine());
+                
+                #region contar divisibilidades
+
+                int divisibilidades = 0;
+                for (int n = 1; n<= numero; n++) 
+                {
+                    if (numero % n == 0)
+                    {
+                        divisibilidades++;
+                    }
+                }
+            }
+        }
+    }
+}
